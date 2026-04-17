f4934fff47ab814614f5bfb891b209e8c65c19a1
Henrique Martins <seu-email-do-github>
2026-04-17 11:38:43 -0300

    Aponta frontend para backend online no Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
+  baseURL: process.env.REACT_APP_API_URL || https://meu-sistema-nps-backend.onrender.com
 });
 
 api.interceptors.request.use((config) => {
